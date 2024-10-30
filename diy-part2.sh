e96f08a502fcc7dbde69ec26cb13958cf4b4589a
jackpang960 <84694135+jackpang960@users.noreply.github.com>
2024-10-30 22:16:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,3 +25,6 @@ git apply $GITHUB_WORKSPACE/patches/*.patch
 # Modify golang version
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+# Remove gost patch
+rm -rf /feeds/kenzo/gost/patches/010-go1-20.patch
